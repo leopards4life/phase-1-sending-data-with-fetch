9fc56d14c1b743246e680e120dcc8e1e8a6fd925
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-01-14 07:09:04 -0700

    Built generic functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle",
+    }),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (obj) {
+        console.log(obj);
+})
+    .catch(function (err) {
+        alert("Bad things!");
+        console.log(err.message);
+    });
+
+    function submitData {
+        
+    }
